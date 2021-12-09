5f78435ebec0f8efb585ed813643876fde607434
Davis2020 <55894495+Davis2020@users.noreply.github.com>
2021-12-09 06:26:12 -0500

    Update CDS_302_DATABASE_UI.py

--- CDS_302_DATABASE_UI.py
+++ CDS_302_DATABASE_UI.py
@@ -11,7 +11,7 @@ import streamlit as st
 import sqlite3 as sql
 
 def app():
-    conn = sql.connect('https://drive.google.com/file/d/1_CZJan0aM2ppVMlpglTIT8pMQVrx_HVx/view?usp=sharing')
+    conn = sql.connect('https://github.com/Davis2020/streamlit_heroku/blob/0c7e13fd5ba9a6fa73e36f46ff2662a5a377a595/Levels_FYI.db')
     comp_query = "select company_name from company"
     title_query = "select title from employee"
     title_df = pd.read_sql(title_query, conn)
