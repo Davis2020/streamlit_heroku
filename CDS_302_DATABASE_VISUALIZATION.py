37d2797541a535fd91718755e2053e771356556f
Davis2020 <55894495+Davis2020@users.noreply.github.com>
2021-12-10 03:05:23 -0500

    Update CDS_302_DATABASE_VISUALIZATION.py

--- CDS_302_DATABASE_VISUALIZATION.py
+++ CDS_302_DATABASE_VISUALIZATION.py
@@ -162,8 +162,8 @@ def app():
     
     # Use Local CSS File
     def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+        with open(file_name) as f:
+            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
     local_css("style/style.css")
